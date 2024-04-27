3354c3c9bec55da8379316abc8f85ff0796f6e22
JaCrawford92 <cckenny9@gmail.com>
2024-04-27 16:25:36 -0500

    updated css and templates

--- catcollector_app/views.py
+++ catcollector_app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+# from django.http import HttpResponse
+
+# Add this cats list below the imports
+cats = [
+  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+]
 
 # Create your views here.
+def home(request):
+    # res.send('hello') in express
+    # return HttpResponse('hello')
+    return render(request, 'cats/home.html')
+
+def about(request):
+    return render(request, 'cats/about.html')
+
+def cats_index(request):
+    # passing data extremely similar to EJS
+    return render(request, 'cats/index.html', {
+        'cats': cats
+    })
