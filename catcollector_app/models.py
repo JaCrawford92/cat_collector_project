c525b6975b6e51348de4ee413cd2f635fed35a54
JaCrawford92 <cckenny9@gmail.com>
2024-05-04 16:14:34 -0500

    models added

--- catcollector_app/models.py
+++ catcollector_app/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.urls import reverse
+
+# choices for cat's feeding schedule
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('toys_detail', kwargs={'pk': self.id})
+
 
 # Create your models here.
+class Cat(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'cat_id': self.id})
+    
+
+class Feeding(models.Model):
+    date = models.DateField('feeding Date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+    # on delete they will remove all Feeding instances related to the deleted cat
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        # get_Field_display() returns a human readable value for the field
+        return f"{self.get_meal_display()} on {self.date}"
+    
+class Meta:
+    ordering = ['-date']
--- catcollector_app/views.py
+++ catcollector_app/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # from django.http import HttpResponse
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Cat
+from .forms import FeedingForm
 # Add this cats list below the imports
-cats = [
-  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
-  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
-]
+# cats = [
+#   {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+#   {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+# ]
 
 # Create your views here.
 def home(request):
@@ -17,7 +19,42 @@ def about(request):
     return render(request, 'cats/about.html')
 
 def cats_index(request):
+    cats = Cat.objects.all()
     # passing data extremely similar to EJS
     return render(request, 'cats/index.html', {
         'cats': cats
     })
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    # making an instance of the FeedingForm class
+    feeding_form = FeedingForm()
+    return render(request, 'cats/detail.html', {
+        'cat': cat, 'feeding_form': FeedingForm
+    })
+
+def add_feeding(request, pk):
+    # create a ModelForm instance with POST data
+    # this is like req.body in express
+    form = FeedingForm(request.POST)
+    # validate the form
+    if form.is_valid():
+        # dont save the form until it has our cat_id assigned
+        new_feeding = form.save(commit=False)
+        # date, meal, cat --> cat_id is django magic
+        new_feeding.cat_id = pk
+        new_feeding.save()
+    return redirect('detail', cat_id=pk)
+
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = ['name', 'breed', 'description', 'age']
+
+class CatUpdate(UpdateView):
+    model = Cat
+    fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
